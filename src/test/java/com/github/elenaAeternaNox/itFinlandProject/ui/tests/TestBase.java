dc0156ac2ef29fd446fc00d675c5da0e32cc605c
elenaAeternanox <antares87@mail.ru>
2021-12-11 12:21:59 +0100

    Fix allure attachments

--- src/test/java/com/github/elenaAeternaNox/itFinlandProject/ui/tests/TestBase.java
+++ src/test/java/com/github/elenaAeternaNox/itFinlandProject/ui/tests/TestBase.java
@@ -1,12 +1,10 @@
 package com.github.elenaAeternaNox.itFinlandProject.ui.tests;
 
-import com.github.elenaAeternaNox.itFinlandProject.ui.steps.ItFinlandSteps;
-import com.github.elenaAeternaNox.itFinlandProject.ui.config.Project;
-import com.github.elenaAeternaNox.itFinlandProject.ui.helpers.AllureAttachments;
-import com.github.elenaAeternaNox.itFinlandProject.ui.helpers.DriverSettings;
-import com.github.elenaAeternaNox.itFinlandProject.ui.helpers.DriverUtils;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.github.elenaAeternaNox.itFinlandProject.ui.helpers.AllureAttachments;
+import com.github.elenaAeternaNox.itFinlandProject.ui.helpers.DriverSettings;
+import com.github.elenaAeternaNox.itFinlandProject.ui.steps.ItFinlandSteps;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,16 +21,10 @@ public class TestBase {
 
     @AfterEach
     public void addAttachments() {
-        String sessionId = DriverUtils.getSessionId();
-
-        AllureAttachments.addScreenshotAs("Last screenshot");
-        AllureAttachments.addPageSource();
-        AllureAttachments.addBrowserConsoleLogs();
-
+        AllureAttachments.screenshotAs("Last screenshot");
+        AllureAttachments.pageSource();
+        AllureAttachments.browserConsoleLogs();
+        AllureAttachments.addVideo();
         Selenide.closeWebDriver();
-
-        if (Project.isVideoOn()) {
-            AllureAttachments.addVideo(sessionId);
-        }
     }
 }
